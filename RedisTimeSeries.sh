2d24c3b9d779829d1eaa3070b129bd0e6a0bbc6d
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2023-08-21 12:38:20 +0900

    Don't use -j N option for RedisTimeSeries.sh

--- RedisTimeSeries.sh
+++ RedisTimeSeries.sh
@@ -2,6 +2,7 @@ HOME=/opt/nestdaq
 git clone --recursive https://github.com/RedisTimeSeries/RedisTimeSeries.git
 cd RedisTimeSeries
 git checkout -b v1.4.9 v1.4.9
-make -j6
+# make -j N causes error
+make
 mkdir -p $HOME/lib
 cp bin/linux-x64-release/redistimeseries.so $HOME/lib
